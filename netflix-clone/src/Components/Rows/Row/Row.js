d0c4bf30f508cfd0f47c8247737a58f2663ad1ac
angularacsi <walelignbalew2013@gmail.com>
2025-05-18 20:35:10 +0300

    Building rows and trailer

--- netflix-clone/src/Components/Rows/Row/Row.js
+++ netflix-clone/src/Components/Rows/Row/Row.js
@@ -2,10 +2,13 @@ import React from 'react'
 import axios from "../../../Utils/Axios"  
 import requests from '../../../Utils/Requests'
 import { useEffect, useState } from 'react'
-import './row.css'
+import movieTrailer from 'movie-trailer'
+import ReactYoutube from 'react-youtube'
+ import './row.css'
 
 const Row = ({title,fetchUrl,isLargeRow}) => {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
   const base_url = "https://image.tmdb.org/t/p/original/";
   useEffect(() => {
     async function fetchData() {
@@ -16,7 +19,21 @@ const Row = ({title,fetchUrl,isLargeRow}) => {
       return request
     }
     fetchData()
-  }, [ ])
+  }, [fetchUrl])
+
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || movie?.title || movie?.original_name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  }
 
 
   
@@ -28,12 +45,24 @@ const Row = ({title,fetchUrl,isLargeRow}) => {
         <div className="row_posters">
           {movies.map((movie) => (
             <img
+            onClick={() => {handleClick(movie) }}
               key={movie.id}
               className={`row_poster ${isLargeRow && "row_posterLarge"}`}
               src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
           ))}
         </div>
       </div>
+      <div>
+      {trailerUrl && <ReactYoutube videoId={trailerUrl} opts={{
+        height: "390",
+        width: "100%",
+        playerVars: {
+          autoplay: 1,
+        },
+      }} />}
+      </div>
+       
+
       
 
 
--- netflix-clone/src/Components/Rows/RowList/RowList.js
+++ netflix-clone/src/Components/Rows/RowList/RowList.js
@@ -15,7 +15,115 @@ const RowList = () => {
     <Row  
    title="NETFLIX ORIGINALS"
    fetchUrl={requests.fetchNetflixOriginals}
+    isLargeRow={true}
+
       />
+    <Row
+   title="Trending Now"
+    fetchUrl={requests.fetchTrending}
+    isLargeRow={false}
+    />
+    <Row
+   title="Top Rated"
+    fetchUrl={requests.fetchTopRated}
+    isLargeRow={false}
+    />
+    <Row  
+    title="Action Movies"
+    fetchUrl={requests.fetchActionMovies}
+    isLargeRow={false}
+    />
+    <Row  
+    title="Comedy Movies"
+    fetchUrl={requests.fetchComedyMovies}
+    isLargeRow={false}
+    />
+    <Row
+    title="Horror Movies"
+    fetchUrl={requests.fetchHorrorMovies}
+    isLargeRow={false}
+    />
+    <Row
+    title="Romance Movies"
+    fetchUrl={requests.fetchRomanceMovies}
+    isLargeRow={false}
+    />
+    <Row
+    title="Documentaries"
+    fetchUrl={requests.fetchDocumentaries}
+    isLargeRow={false}
+    />
+    {/* <Row
+    title="Science Fiction Movies"
+    fetchUrl={requests.fetchScienceFictionMovies} 
+    isLargeRow={false}
+    />
+    <Row
+    title="Fantasy Movies"
+    fetchUrl={requests.fetchFantasyMovies}
+    isLargeRow={false}
+    />
+    <Row
+    title="Mystery Movies"
+    fetchUrl={requests.fetchMysteryMovies}
+    isLargeRow={false}
+    />
+    <Row
+    title="Western Movies"
+    fetchUrl={requests.fetchWesternMovies}
+    isLargeRow={false}
+    />
+    <Row
+    title="War Movies"
+    fetchUrl={requests.fetchWarMovies}
+    isLargeRow={false}
+    />
+    <Row
+    title="History Movies"
+    fetchUrl={requests.fetchHistoryMovies}
+    isLargeRow={false}
+    />
+    <Row
+    title="Crime Movies"
+    fetchUrl={requests.fetchCrimeMovies}
+    isLargeRow={false}
+    />
+    <Row
+    title="Family Movies"
+    fetchUrl={requests.fetchFamilyMovies}
+    isLargeRow={false}
+    />
+    <Row
+    title="TV Shows"
+    fetchUrl={requests.fetchTvShows}
+    isLargeRow={false}
+    />
+    <Row
+    title="Anime Movies"
+    fetchUrl={requests.fetchAnimeMovies}
+    isLargeRow={false}
+    />
+    <Row  
+    title="Kids Movies"
+    fetchUrl={requests.fetchKidsMovies}
+    isLargeRow={false}
+    />
+    <Row
+    title="Documentary Series"
+    fetchUrl={requests.fetchDocumentarySeries}
+    isLargeRow={false}
+    />
+    <Row
+    title="Short Movies"
+    fetchUrl={requests.fetchShortMovies}
+    isLargeRow={false}
+    />
+    <Row
+    title="Music Movies"
+    fetchUrl={requests.fetchMusicMovies}
+    isLargeRow={false}
+    /> */}
+    
     </>
   )
 }
